0668af15bc5b2128a6c01ecd4c2dd41e25a0adca
TaeminHan0302 <tefzen0302@gmail.com>
2026-03-13 09:25:10 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+    int var = 100;
+    printf("int : %?\n", var1);
+    printf("hex : %?\n", var1);
+    printf("oct : %?\n", var1);
 
+    char var2 = 'c';
+    printf("char : %?\n", var2);
+    char* var3 = "Hello World!";
+    printf("string : %?\n", var3);
+
+    float var4 = 3.141592;
+    printf("float : %?\n", var4);
+    printf("exp : %?\n", var4);
+    
     return 0;
 }

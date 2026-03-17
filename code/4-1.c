5c1a836e898675fe4225018f58120d7d558de014
TaeminHan0302 <tefzen0302@gmail.com>
2026-03-17 12:18:03 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -6,9 +6,22 @@ int main()
     int b;
     char c;
     
-    scanf("%d %d %c\n", a, b, c);
-    if(c == "+"){
+    scanf("%d %d %c\n", &a, &b, &c);
+    if(c == '+'){
         a = a + b;
+        printf("%d\n", a);
+    }
+    if(c == '-'){
+        a = a - b;
+        printf("%d\n", a);
+    }
+    if(c == '*'){
+        a = a*b;
+        printf("%d\n", a);
+    }
+    if(c == '/'){
+        a = a/b;
+        printf("%d\n", a);
     }
 
     return 0;

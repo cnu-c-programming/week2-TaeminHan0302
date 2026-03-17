54796abd58ce8c3e8763dc6e056db23bfc77e6cd
TaeminHan0302 <tefzen0302@gmail.com>
2026-03-17 12:36:30 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-
+    int k;
+    for(int i=1; i<10; i++){
+        for(int a  = 1; a < 10; a++){
+            k = a * i;
+            printf("%d * %d = %d\n", i, a, k);
+        }
+    }
     return 0;
 }
 

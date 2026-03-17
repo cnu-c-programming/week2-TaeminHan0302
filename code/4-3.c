4a7cf559d3fb95667482f17ce388e5c1f1e9dc60
TaeminHan0302 <tefzen0302@gmail.com>
2026-03-17 12:51:53 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,6 +2,17 @@
 
 int main()
 {
+    int n;
+    scanf("%d\n", n);
+    if(n%2 == 1){
+        printf("true");
+    }
+    if(n == 2){
+        printf("true");
+    }
+    else{
+        printf("false");
+    }
 
     return 0;
 }
